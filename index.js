546fe6813c54593073091a1c0910e53fefbd570f
foodiemeniac <134602790+foodiemeniac@users.noreply.github.com>
2023-05-25 16:07:02 +0530

    commited change like added files(index,...)

---
+++ index.js
@@ -0,0 +1,20 @@
+// Sample data for menu items
+const iceCreamMenu = [
+    { name: "Vanilla", price: "$3.99" },
+    { name: "Chocolate", price: "$3.99" },
+    { name: "Strawberry", price: "$3.99" }
+  ];
+  
+  const shakeMenu = [
+    { name: "Cookies and Cream", price: "$4.99" },
+    { name: "Chocolate Brownie", price: "$4.99" },
+    { name: "Strawberry Banana", price: "$4.99" }
+  ];
+  
+  const pizzaMenu = [
+    { name: "Margherita", price: "$9.99" },
+    { name: "Pepperoni", price: "$11.99" },
+    { name: "Vegetarian", price: "$10.99" }
+  ];
+  
+  
